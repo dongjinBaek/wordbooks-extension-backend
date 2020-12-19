69ec456e9bc12664f2b5f266f8a73cd998def0eb
dongjinBaek <dj9136@gmail.com>
2020-12-19 09:15:56 -0500

    Init Spring Boot project

---
+++ src/test/java/com/dongjinbaek/wordbooks/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongjinbaek.wordbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
