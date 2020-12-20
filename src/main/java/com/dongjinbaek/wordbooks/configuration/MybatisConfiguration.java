07e0fb7ff3fe4c23e0a4b6cae9426ec59eb709b3
dongjinBaek <dj9136@gmail.com>
2020-12-20 05:07:38 -0500

    mybatis-config.xml 추가

--- src/main/java/com/dongjinbaek/wordbooks/configuration/MybatisConfiguration.java
+++ src/main/java/com/dongjinbaek/wordbooks/configuration/MybatisConfiguration.java
@@ -5,22 +5,24 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
 @MapperScan("com.dongjinbaek.wordbooks.dao")
 public class MybatisConfiguration {
+    @Autowired
+    ApplicationContext applicationContext;
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
 
-        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*Mapper.xml");
-        sessionFactory.setMapperLocations(res);
+        sessionFactory.setConfigLocation(applicationContext.getResource("classpath:mybatis-config.xml"));
+        sessionFactory.setMapperLocations(applicationContext.getResources("classpath:mapper/*Mapper.xml"));
 
         return sessionFactory.getObject();
     }
