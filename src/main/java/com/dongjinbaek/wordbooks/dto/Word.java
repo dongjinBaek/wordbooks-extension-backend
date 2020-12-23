0b19622c43c5f5f4f9ecbec5503e35eed9e7c6d1
dongjinBaek <dj9136@gmail.com>
2020-12-23 09:41:46 -0500

    word get 메소드 추가

--- src/main/java/com/dongjinbaek/wordbooks/dto/Word.java
+++ src/main/java/com/dongjinbaek/wordbooks/dto/Word.java
@@ -1,8 +1,14 @@
 package com.dongjinbaek.wordbooks.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Word {
     private String userId;
     private String english;
--- src/main/java/com/dongjinbaek/wordbooks/mapper/WordMapper.java
+++ src/main/java/com/dongjinbaek/wordbooks/mapper/WordMapper.java
@@ -7,6 +7,9 @@ import com.dongjinbaek.wordbooks.dto.Word;
 import org.apache.ibatis.annotations.Param;
 
 public interface WordMapper {
+
+    Word get(@Param("userId") String userId, @Param("english") String english);
+
     List<Word> search(@Param("userId") String userId, @Param("term") String term);
 
     int addWord(Word word);
